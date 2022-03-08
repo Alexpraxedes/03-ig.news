206216ad6acf63cd1f382ec2fef815a9410a0ad1
unknown <id.praxedes@gmail.com>
2022-03-07 23:43:36 -0300

    feat: subescribe with stripe add

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,27 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
-export function SubscribeButton(){
+interface SubscribeButtonProps{
+    priceId: string;
+}
+
+export function SubscribeButton( { priceId }: SubscribeButtonProps ){
+    const { status }  = useSession();
+
+    function handleSubscribe() {
+        if (! status ){
+            signIn();
+            return;
+        }
+
+        // cria a checkout session
+    }
+
     return(
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
